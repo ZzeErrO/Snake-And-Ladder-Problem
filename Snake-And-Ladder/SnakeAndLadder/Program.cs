5f58a1771f95204070b3989ad259f388eb555cca
prashikjaware <prashikjaware123@gmail.com>
2021-03-07 11:44:10 +0530

    [Prashik]Added: Shows the current starting position of player

---
+++ Snake-And-Ladder/SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int START_POSITION = 0;
+        public const int END_POSITION = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Current Position of Player is: " + START_POSITION );
+            Console.ReadKey();
+        }
+    }
+}
