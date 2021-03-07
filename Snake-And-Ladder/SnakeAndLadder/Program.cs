8636f335670e6f6ae1089163b0616d7c0878b26f
prashikjaware <prashikjaware123@gmail.com>
2021-03-07 15:50:06 +0530

    [Prashik]Added: Player check for a Option of NoPlay, Ladder or Snake

--- Snake-And-Ladder/SnakeAndLadder/Program.cs
+++ Snake-And-Ladder/SnakeAndLadder/Program.cs
@@ -10,6 +10,31 @@ namespace SnakeAndLadder
     {
         public const int START_POSITION = 0;
         public const int END_POSITION = 100;
+
+        public static int NoPlay(int currentPosition) 
+        {
+            return currentPosition;
+        }
+
+        public static int Ladder(int currentPosition, int number)
+        {
+            currentPosition += number;
+            return currentPosition;
+        }
+
+        public static int Snake(int currentPosition, int number)
+        {
+            if (currentPosition != 0)
+            {
+                currentPosition -= number;
+            }
+            if (currentPosition < 0)
+            {
+                currentPosition = 0;
+            }
+            return currentPosition;
+        }
+
         static void Main(string[] args)
         {
             //variable
@@ -17,7 +42,29 @@ namespace SnakeAndLadder
             Console.WriteLine("The  Starting Position of Player is: " + START_POSITION );
             Random Dice = new Random();
             int number = Dice.Next(1,7);
-            currentPosition += number;
+            int checkmoves = Dice.Next(1, 4);
+            Console.WriteLine(number);
+            Console.WriteLine(checkmoves);
+
+            switch (checkmoves)
+            {
+                case 1:
+                    int move = NoPlay(currentPosition);
+                    currentPosition = move;
+                    break;
+                case 2:
+                    int move1 = Ladder(currentPosition, number);
+                    currentPosition = move1;
+                    break;
+                case 3:
+                    int move2 = Snake(currentPosition, number);
+                    currentPosition = move2;
+                    break;
+                default:
+                    Console.WriteLine("No such option");
+                    break;
+            }
+
             Console.WriteLine("The Current Position of Player is: " + currentPosition);
             Console.ReadKey();
         }
